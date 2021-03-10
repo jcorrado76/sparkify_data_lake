2fd902e4fad421e98418e9bb624560f91c5e182e
Joseph Corrado <josephcorrado7@gmail.com>
2021-03-10 16:50:58 -0500

    added a shell script to push the spark data tables into S3

---
+++ push_tables_to_s3.sh
@@ -0,0 +1,5 @@
+s3-dist-cp --src /user/sparkify_data/users --dest s3://udacity-data-engineer-data-lake-project/users  
+s3-dist-cp --src /user/sparkify_data/time --dest s3://udacity-data-engineer-data-lake-project/time  
+s3-dist-cp --src /user/sparkify_data/songplays --dest s3://udacity-data-engineer-data-lake-project/songplays  
+s3-dist-cp --src /user/sparkify_data/artists --dest s3://udacity-data-engineer-data-lake-project/artists  
+s3-dist-cp --src /user/sparkify_data/songs --dest s3://udacity-data-engineer-data-lake-project/songs  
